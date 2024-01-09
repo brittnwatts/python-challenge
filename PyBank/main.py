8764d03468e90502b71a7f2faa0083807a5e26c3
brittnwatts <bwatts0721@gmail.com>
2024-01-09 01:12:20 -0500

    Finished PyBank and PyPoll and corresponding analysis.txt output files

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,63 +4,60 @@ import csv
 import pandas as pd
 
 # Set path to csv
-
 csvpath = os.path.join('Resources', 'budget_data.csv')
-
-# Translates the file into a readable format to be used by python
 with open (csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
-# Reads the first row of headers and prints the headers as actual headers in the output
+# Reads the first row of headers and skips the first row
     csv_header = next(csvreader)
-
+# Set file path for txt
+    analysis_file = ("Analysis\PyBank_analysis.txt")
 #Lists to store values 
     date = []
     newDate = []
     newStringVal = []
     avgList = []
-# Reads each row of data after the header
-    for row in csvreader:
+# opens file path and determines that code must be run while open so it will print the output to txt_file
+    with open(analysis_file, 'w', newline = '') as txt_file:  
+        txt_file.write(f"\nFinancial Analysis\n\n")
+        txt_file.write(f"-------------------------\n\n")
+    # For row in csv reader, do calculations and print to terminal and txt_file
+        for row in csvreader:
         # Add data to the new list(it is printed as a string so you have to convert later)
-        newStringVal.append(row[1])
+            newStringVal.append(row[1])
         # Add data to the date array(Will stay a String)
-        date.append(row[0]) 
-
-#Supposed to print to the txt file.
-file = 'Analysis/analysis.txt'
-with open(file, 'w', newline = '') as txt_file:  
-    txt_file.write("This is an entry")
+            date.append(row[0]) 
 #Print header to output 
-    print("Financial Analysis")
-    txt_file.write("Financial Analysis")
-    print("-------------------")
-    txt_file.write("-------------------")
-
+        print(f"\nFinancial Analysis\n")
+        print(f"-----------------------\n")
 # Count the months in date.
-    print(f'Total Months:    {len(date)}')
+        print(f'Total Months: {len(date)}\n')
+        txt_file.write(f'Total Months: {len(date)}\n\n')
 # Create a newintlist to store integer values from String list
-NewIntList = [eval(i) for i in newStringVal]
+        NewIntList = [eval(i) for i in newStringVal]
 # Add all values together and print total
-totalprofits = sum(NewIntList)
+        totalprofits = sum(NewIntList)
 # Print Total Profits
-print("Total:           $",totalprofits)
+        print(f"Total: ${totalprofits}\n")
+        txt_file.write(f'Total: ${totalprofits}\n\n')
 # Calculate differences between values in newIntList. This iterates through the list and finds the difference btwn adjacent vals
-avgList = [NewIntList[i+1] - NewIntList[i] for i in range(len(NewIntList)-1)]
+        avgList = [NewIntList[i+1] - NewIntList[i] for i in range(len(NewIntList)-1)]
 # Declare a variable for the average change
-AvgChng = sum(avgList)/len(avgList)
+        AvgChng = sum(avgList)/len(avgList)
 # Print into the Terminal and round to cents
-print(f'Average Changes: $, {round(AvgChng, 2)}')
-
-
+        print(f'Average Changes: ${round(AvgChng, 2)}\n')
+        txt_file.write(f'Average Changes: ${round(AvgChng, 2)}\n\n')
 # Zipped lists into summary with variables date and NewIntList
-summary =  list(zip(date, NewIntList))
+        summary =  list(zip(date, NewIntList))
 # For loop that will append into newDate , but skipping the first value
-for i in range(1, len(summary)):
-    newDate.append(date[i])
+        for i in range(1, len(summary)):
+            newDate.append(date[i])
 # Zipped list into new summary with variables newDate and avgList
-newSummary = list(zip(newDate, avgList))
+        newSummary = list(zip(newDate, avgList))
 # Sets a variable to call the newDate key separate from the avgList Key, but still attached to max/min.
-maxSummary = max(newSummary, key=lambda x: x[1])
-minSummary = min(newSummary, key = lambda x: x[1])
-# FINAL PRINT LINES
-print(f"Greatest Increase in Profits: {maxSummary[0]}, ${max(avgList)}")
-print(f"Greatest Decrease in Profits: {minSummary[0]}, ${min(avgList)}")
+        maxSummary = max(newSummary, key=lambda x: x[1])
+        minSummary = min(newSummary, key = lambda x: x[1])
+# FINAL PRINT LINES!!!!!
+        print(f"Greatest Increase in Profits: {maxSummary[0]} (${max(avgList)})\n")
+        print(f"Greatest Decrease in Profits: {minSummary[0]} (${min(avgList)})\n")
+        txt_file.write(f"Greatest Increase in Profits: {maxSummary[0]} (${max(avgList)})\n\n")
+        txt_file.write(f"Greatest Decrease in Profits: {minSummary[0]} (${min(avgList)})")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,11 +2,13 @@ import os
 import csv
 import pandas as pd
 
+# Set Path to csv and do calculations within the open file.
 csvpath = os.path.join('Resources', 'election_data.csv')
-
 with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
     csv_header = next(csvreader)
+# Set file path for text
+    analysis_file = ("Analysis/PyPoll_analysis.txt")
 # Create open lists to store items
     ballotID = []
     county = []
@@ -15,50 +17,61 @@ with open(csvpath, 'r') as csvfile:
     stockham = 'Charles Casper Stockham'
     degette = "Diana DeGette"
     doane = "Raymon Anthony Doane"
+## opens file path and determines that code must be run while open so it will print the output to txt_file
+    with open(analysis_file, 'w', newline ='') as txt_file:  
 # For loop for iterating through rows in CSV file
-    for row in csvreader:
+        for row in csvreader:
         # Append lists with csv data so you can work with it
-        ballotID.append(row[0])
-        county.append(row[1])
-        candidate.append(row[2])
-file = 'Analysis/analysis.txt'
-with open(file, 'w', newline = '') as txt_file:  
-    txt_file.write("This is an entry")
+            ballotID.append(row[0])
+            county.append(row[1])
+            candidate.append(row[2])
+
 # Create a variable to count the number of votes and print
-countRow = len(ballotID)
-print("Election Results")
-print("----------------------------")
-print(f"Total Votes:   ", countRow)
-print("----------------------------")
+        countRow = len(ballotID)
+        print(f"\nElection Results\n")
+        print(f"----------------------------\n")
+        print(f"Total Votes:   {countRow}\n")
+        print(f"----------------------------\n")
+        txt_file.write(f"\nElection Results\n\n")
+        txt_file.write(f"----------------------------\n\n")
+        txt_file.write(f"Total Votes:   {countRow}\n\n")
+        txt_file.write(f"----------------------------\n\n")
 
 #Candidates if statements to check the length of their votes compared to the length of the whole
-total = len(candidate)
-count = candidate.count(stockham)
-if count > 0:
-    percentage = round(((count/total) * 100), 3)
-    print(f'{stockham}: {percentage}% ({count})')
+        total = len(candidate)
+        count = candidate.count(stockham)
+        if count > 0:
+            percentage = round(((count/total) * 100), 3)
+            print(f'{stockham}: {percentage}% ({count})\n')
+            txt_file.write(f'{stockham}: {percentage}% ({count})\n\n')
 # Separate if statement where count now = count of DeGette
-count = candidate.count(degette)
-if count > 0:
-    percentage = round(((count/total) * 100), 3)
-    print(f'{degette}: {percentage}% ({count})')
+        count = candidate.count(degette)
+        if count > 0:
+            percentage = round(((count/total) * 100), 3)
+            print(f'{degette}: {percentage}% ({count})\n')
+            txt_file.write(f'{degette}: {percentage}% ({count})\n\n')
 # "", but with Doane
-count = candidate.count(doane)
-if count > 0:
-    percentage = round(((count/total) * 100), 3)
-    print(f'{doane}: {percentage}% ({count})')
+        count = candidate.count(doane)
+        if count > 0:
+            percentage = round(((count/total) * 100), 3)
+            print(f'{doane}: {percentage}% ({count})\n')
+            txt_file.write(f'{doane}: {percentage}% ({count})\n\n')
 
-print("----------------------------")
-
+        print(f"----------------------------\n")
+        txt_file.write(f"----------------------------\n\n")
 # This sets variables to candidate count based off of the candidates name
-winnerStockham = candidate.count(stockham)
-winnerDeGette = candidate.count(degette)
-winnerDoane = candidate.count(doane)
+        winnerStockham = candidate.count(stockham)
+        winnerDeGette = candidate.count(degette)
+        winnerDoane = candidate.count(doane)
 # If statement that compares the count of their names through winner varaible.
-if winnerStockham > winnerDeGette and winnerStockham > winnerDoane:
-    print("Winner: Charles Casper Stockham")
-elif winnerDeGette > winnerStockham and winnerDeGette > winnerDoane:
-    print("Winner: Diana DeGette")
-elif  winnerDoane > winnerDeGette and winnerDoane > winnerStockham:
-    print("Winner: Raymon Anthony Doane")
-print("----------------------------")
+        if winnerStockham > winnerDeGette and winnerStockham > winnerDoane:
+            print(f"Winner: Charles Casper Stockham\n")
+            txt_file.write(f"Winner: Charles Casper Stockham\n\n")
+        elif winnerDeGette > winnerStockham and winnerDeGette > winnerDoane:
+            print(f"Winner: Diana DeGette\n")
+            txt_file.write(f"Winner: Diana DeGette\n\n")
+        elif  winnerDoane > winnerDeGette and winnerDoane > winnerStockham:
+            print(f"Winner: Raymon Anthony Doane\n")
+            txt_file.write(f"Winner: Raymon Anthony Doane\n\n")
+        print(f"----------------------------")
+        txt_file.write(f"----------------------------")
